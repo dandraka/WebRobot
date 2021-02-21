bc4f5b05a8d758b53345d0fb57843c3e9e9e3322
Jim (Dimitrios) Andrakakis <dimitrios.andrakakis@elipslife.com>
2021-02-21 17:32:49 +0100

    Add project files.

---
+++ RobotAPI/Controllers/RobotController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RobotBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RobotAPI.Controllers
+{
+    public class Delta
+    {
+        public int DeltaX;
+        public int DeltaY;
+
+        public override string ToString()
+        {
+            return $"{this.DeltaX},{this.DeltaY}";
+        }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class RobotController : ControllerBase
+    {
+        private static Robot _robot;
+
+        private readonly ILogger<RobotController> _logger;
+
+        public RobotController(ILogger<RobotController> logger)
+        {
+            _logger = logger;
+
+            if (_robot == null)
+            {
+                _robot = new Robot()
+                {
+                    Name = "XR-" + (new Random()).Next(100, 999).ToString(),
+                    SizeX = (new Random()).Next(1, 9) * 10,
+                    SizeY = (new Random()).Next(1, 9) * 10
+                };
+            }
+        }
+
+        // https://localhost:44383/robot?deltax=15&deltay=-10
+        [HttpGet]
+        public Robot Get(int? deltaX, int? deltaY)
+        {
+            if (deltaX.HasValue || deltaY.HasValue)
+            {
+                _robot.Move(deltaX.GetValueOrDefault(0), deltaY.GetValueOrDefault(0));
+            }
+            return _robot;
+        }
+    }
+}
---
+++ RobotBase/Robot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+
+namespace RobotBase
+{
+    public class Robot
+    {
+        public string Name { get; set; }
+
+        public int SizeX { get; set; }
+
+        public int SizeY { get; set; }
+
+        public int PosX { get; set; }
+
+        public int PosY { get; set; }
+
+        public int DistanceFromObstacle { get; set; }
+
+        public Point CornerTopLeft { get => new Point(PosX, PosY); }
+
+        public Point CornerTopRight { get => new Point(PosX + SizeX, PosY); }
+
+        public Point CornerBottomLeft { get => new Point(PosX, PosY + SizeY); }
+
+        public Point CornerBottomRight { get => new Point(PosX + SizeX, PosY + SizeY); }
+
+        public void Move(int deltaX, int deltaY)
+        {
+            this.PosX += deltaX;
+            this.PosY += deltaY;
+        }
+
+    }
+}
